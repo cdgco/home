0441b0e349ce873669203e5b379589ba19c6ee26
CDG <cdgco@users.noreply.github.com>
2017-06-28 14:36:53 -0700

    Favicons

--- index.php
+++ index.php
@@ -7,10 +7,21 @@ else { header( 'Location: setup.html' );}
 <html>
 
 <head>
-     <meta charset="UTF-8">
-    <meta name="google" content="notranslate">
-    <meta http-equiv="Content-Language" content="en">
+    
+<meta charset="UTF-8">
+<meta name="google" content="notranslate">
+<meta http-equiv="Content-Language" content="en">
 <base target="_parent">
+    
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/manifest.json">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="favicon/favicon.ico">
+<meta name="msapplication-config" content="favicon/browserconfig.xml">
+<meta name="theme-color" content="#f5f5f5">
+    
     <title>CDG Home</title>
 <script>
 if(self==top) {
--- s/80.php
+++ s/80.php
@@ -1,3 +1,11 @@
+<link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+<link rel="manifest" href="../favicon/manifest.json">
+<link rel="mask-icon" href="../favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="../favicon/favicon.ico">
+<meta name="msapplication-config" content="../favicon/browserconfig.xml">
+<meta name="theme-color" content="#f5f5f5">
 <title>CDG Home</title>
 <style>
     #frame { position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:125%; height:125%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999; }
